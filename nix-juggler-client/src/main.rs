232aaf81e4ff49174ac0812cee89dcf135124ba5
BurningTurtle <git@burningturtle.win>
2026-09-19 02:40:47 +0200

    cargo workspace

--- nix-juggler-client/src/main.rs
+++ nix-juggler-client/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use ::std::process::Command;
+
+fn main() -> std::io::Result<()> {
+    let child = Command::new("ls").arg("-l").arg("-a").spawn()?;
+
+    Ok(())
 }
