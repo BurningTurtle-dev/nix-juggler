5f2ff90a72ddbb69f262a91599508c64a64d60a9
BurningTurtle <git@burningturtle.win>
2026-09-20 20:38:41 +0200

    locate the writer bin during cargo run

--- nix-juggler-client/src/main.rs
+++ nix-juggler-client/src/main.rs
@@ -3,7 +3,11 @@ use std::env::args;
 use std::process::{Command, Stdio};
 
 fn spawn_writer(args: VecDeque<String>) -> std::io::Result<std::process::Child> {
-    let child = Command::new("./nix-juggler-writer")
+    // Locate the writer binary next to this binary
+    let exe = std::env::current_exe()?;
+    let writer_path = exe.with_file_name("nix-juggler-writer");
+
+    let child = Command::new(writer_path)
         .args(&args) // .args() takes IntoIterator<Item = AsRef<OsStr>>, Vec<String> works directly
         .stdin(Stdio::piped())
         //.stdout(Stdio::piped())
